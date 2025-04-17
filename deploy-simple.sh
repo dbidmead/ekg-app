d8bdb57104cf85054851d54c0a3c31f3c2fe7703
Drew Bidmead <dmbidmead@gmail.com>
2025-04-17 10:03:43 -0400

    More deployment debugging

--- deploy-simple.sh
+++ deploy-simple.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 
-# Build the project with relative paths
+# Build the project 
 echo "Building project..."
 npm run build
 
 # Create .nojekyll file to disable Jekyll processing
 touch dist/.nojekyll
-
-# Add empty 404.html which will redirect to index.html
-cat > dist/404.html << EOL
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="refresh" content="0;url=./">
-  <title>Redirecting...</title>
-</head>
-<body>
-  <p>If you are not redirected automatically, <a href="./">click here</a>.</p>
-</body>
-</html>
-EOL
 
 # Deploy to GitHub Pages
 echo "Deploying to GitHub Pages..."
